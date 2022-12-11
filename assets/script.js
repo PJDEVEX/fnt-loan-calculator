974415f4c7757ddee7dd19663e18197aa5c893c1
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2022-12-11 15:35:48 +0000

    Fix: link loan amount to chart for correct functioning

--- assets/script.js
+++ assets/script.js
@@ -30,7 +30,7 @@ let checkValues = () => {
     let regexNumber = /^[0-9]+$/;
 
     // Validation expression - loan amount input
-    if(!loanAmountValue.match(regexNumber)) {
+    if (!loanAmountValue.match(regexNumber)) {
         alert(`You entered ${loanAmountInput.value} as LOAN AMOUNT. It shall be a number (e.g.: 10000). Kindly recheck & enter!`);
     }
     // Validation expression - duration input
@@ -41,8 +41,9 @@ let checkValues = () => {
     // Regualar expression for decimal value 
     let regexDecimalNumber = /^\d+\.\d+$/;
 
-    if(!interestRateValue.match(regexDecimalNumber)) {
-        alert(`You entered ${interestRateInput.value} as INTEREST RATE. It shall be a number (e.g.: 5.00). Kindly recheck & enter!`);
+    // Validation expression - interest rate
+    if (!interestRateValue.match(regexDecimalNumber)) {
+        alert(`You entered ${interestRateInput.value} as INTEREST RATE. It shall be like, e.g.: 5.00. Kindly recheck & enter!`);
     }
 
 };
@@ -50,30 +51,29 @@ let checkValues = () => {
 // Function to display chart
 let displayChart = (totalInterestPayable, loanAmount) => {
     const ctx = document.getElementById('myChart').getContext('2d');
-myChart = new Chart(ctx, {
-    type: 'pie',
-    data: {
-        labels: ["Total Interest", "Principal Loan Amount"],
-        datasets: [
-            {
+    myChart = new Chart(ctx, {
+        type: 'pie',
+        data: {
+            labels: ["Total Interest", "Loan Amount"],
+            datasets: [{
                 data: [totalInterestPayable, loanAmount],
                 backgroundColor: ["#0D0D0D", "#0777D9"],
-            
-            borderWidth: 0
-        }],
-    },
-});
+
+                borderWidth: 0
+            }],
+        },
+    });
 };
 
 // Function to update the chart
 let updateChart = (totalInterestPayable, loanAmount) => {
     myChart.data.datasets[0].data[0] = totalInterestPayable;
-    myChart.data.datasets[0].data[1] = totalAmount;   
+    myChart.data.datasets[0].data[1] = loanAmount;
     myChart.update();
 };
 // Call Calculate monthly premium function 
 let calculateMonthlyPremium = () => {
-    
+
     // Call check value function
     checkValues();
 
@@ -99,10 +99,10 @@ let updateData = (monthlyPremium) => {
     // Call update chart function
     if (myChart) {
         updateChart(totalInterestPayable, loanAmount);
-    }   else {
+    } else {
         // Call diplay chart function
         displayChart(totalInterestPayable, loanAmount);
-    }   
+    }
 };
 
 // Refresh input values
@@ -110,7 +110,7 @@ let refreshInputValues = () => {
     loanAmount = parseFloat(loanAmountInput.value);
     interestRate = parseFloat(interestRateInput.value);
     loanPeriod = parseFloat(loanPeriodInput.value);
-    interest = interestRate / 12 / 100; 
+    interest = interestRate / 12 / 100;
 };
 
 // Call for monthly premium function
